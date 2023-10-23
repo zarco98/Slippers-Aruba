d523aa5dd9e18005be850e0922ba03323a225756
Felipe Fonseca <kf.fonseca10@uniandes.edu.co>
2023-10-22 22:28:56 -0500

    Enrutado basico para productos

--- back/app.js
+++ back/app.js
@@ -1,4 +1,11 @@
 const express=require("express");
 const app =express();
 
+app.use(express.json());
+
+//Importar rutas
+const productos=require("./routes/products")
+
+app.use('/api', productos) //Sujeto a decisión (ruta del navegador)
+
 module.exports=app
